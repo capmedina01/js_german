c304d2a32aff9932160b1a66eac175638af66da8
Carlos <carlos_aperdomo@soy.sena.edu.co>
2024-09-01 14:51:09 -0500

    actualizacion

---
+++ acumuladores Ciclos/ciclos.js
@@ -0,0 +1,11 @@
+// ciclo while
+
+const n = parseInt(prompt("Ingrese el número de notas"))
+let acumulado = 0
+let contador = 1
+
+while(contador <= n){
+    let nota = parseFloat(prompt("Ingrese "))
+    contador ++
+    console.log(contador)
+}
